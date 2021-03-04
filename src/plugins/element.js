2bf545cf670d217396f14e0b42722757ed909341
gp754247907 <754247907@qq.com>
2021-03-04 11:03:15 +0800

    提交了分类功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -27,8 +27,12 @@ import {
   Tag,
   Tree,
   Select,
-  Option
+  Option,
+  Cascader
 } from 'element-ui'
+
+
+
 Vue.use(Tree)
 Vue.use(Button)
 Vue.use(Form)
@@ -55,6 +59,7 @@ Vue.use(Dialog)
 Vue.use(Tag)
 Vue.use(Select)
 Vue.use(Option)
+Vue.use(Cascader)
 
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
